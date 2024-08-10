535616874f995d8f165acc08055ed93da0f91006
reham-ahmed68 <rehamdabei@gmail.com>
2024-08-10 16:16:37 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let z=document.getElementById("btn1")
+document.getElementById("btn1").onclick=function(){
+    alert("تم نسخ النص بنجاح");
+}
+let x=document.getElementById("btn2")
+document.getElementById("btn2").onclick=function(){
+    alert("تم نسخ النص بنجاح");
+}
+let y=document.getElementById("btn3")
+document.getElementById("btn3").onclick=function(){
+    alert("تم نسخ النص بنجاح");
+}
